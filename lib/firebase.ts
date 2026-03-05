e9bacdf1968d018fcade8e389e9ffab16ba5a3a8
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2026-03-05 13:09:20 +0100

    Create lib/firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCV1AhhjueVqy0dOmkt53uBkCNtsVC4lTc",
+  authDomain: "crestpoint-capital.firebaseapp.com",
+  databaseURL: "https://crestpoint-capital-default-rtdb.firebaseio.com",
+  projectId: "crestpoint-capital",
+  storageBucket: "crestpoint-capital.firebasestorage.app",
+  messagingSenderId: "204055812881",
+  appId: "1:204055812881:web:5a9d07c515c484229bcab0",
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const auth = getAuth(app);
+export default app;
